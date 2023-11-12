ea52e51fb88fe85a1b75f5541a33853a3f200da7
Eldar Bukeyev <bukeevel@gmail.com>
2023-11-12 12:57:35 +0300

    Добавил задачи

---
+++ ExercisesConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+bool Task1(int money)
+{
+    // У кассира всего 5000 рублей и все эти деньги - монеты по 5 рублей.
+    // Узнать может ли он разменять money покупателя
+    throw new NotImplementedException();
+}
+
+bool Task2(int[] ageArray)
+{
+    // На вход получаем массив возрастов компании отдыхающих. Заведение 18+. Определить все ли ее участники могут быть в заведении
+    throw new NotImplementedException();
+}
